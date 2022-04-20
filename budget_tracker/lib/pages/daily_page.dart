86f9136d45657aa3984f979e25769b7fd8e0cbd3
naneps <ekaprasetya2244@gmail.com>
2022-04-20 12:14:04 +0700

    daily transaction page

--- budget_tracker/lib/pages/daily_page.dart
+++ budget_tracker/lib/pages/daily_page.dart
@@ -1,3 +1,4 @@
+import 'package:budget_tracker/data/daily_json.dart';
 import 'package:budget_tracker/data/day_month.dart';
 import 'package:budget_tracker/theme/colors.dart';
 import 'package:flutter/material.dart';
@@ -57,8 +58,10 @@ class _DailyPageState extends State<DailyPage> {
                     height: 25,
                   ),
                   Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: List.generate(days.length, (index) {
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(
+                      days.length,
+                      (index) {
                         return GestureDetector(
                           onTap: () {
                             setState(() {
@@ -93,20 +96,146 @@ class _DailyPageState extends State<DailyPage> {
                                   child: Center(
                                     child: Text(
                                       days[index]['day'],
-                                      style:
-                                          TextStyle(fontSize: 10, color: white),
+                                      style: TextStyle(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.w600,
+                                          color: activeDay == index
+                                              ? white
+                                              : black),
                                     ),
                                   ),
-                                )
+                                ),
                               ],
                             ),
                           ),
                         );
-                      }))
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Column(
+              children: List.generate(daily.length, (index) {
+                return Column(
+                  children: [
+                    Row(
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: (size.width - 40) * 0.7,
+                          // color: Colors.black,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: grey.withOpacity(0.1),
+                                    shape: BoxShape.circle),
+                                child: Center(
+                                  child: Image.asset(
+                                    daily[index]['icon'],
+                                    height: 30,
+                                    width: 30,
+                                    // scale: 1.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                width: (size.width - 90) * 0.5,
+                                // color: Colors.black,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      daily[index]['name'],
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: black,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      daily[index]['name'],
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: black,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          // width: 30,
+                          // color: Colors.black,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                daily[index]['price'],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 15,
+                                    color: Colors.green),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 65, top: 8),
+                      child: Divider(thickness: 0.8),
+                    )
+                  ],
+                );
+              }),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Total",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: black.withOpacity(0.4)),
+                  ),
+                  Text(
+                    "\$1700.00",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  )
+                ]),
+          )
         ],
       ),
     );
